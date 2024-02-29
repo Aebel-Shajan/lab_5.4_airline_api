7609f43df49648e50df9b78ad7403e9e7ebec8b4
Aebel Shajan <aebelshajan@gmail.com>
2024-02-29 17:09:10 +0000

    Create 2 flights and save them to the flight repository.

--- src/main/java/com/example/airline_api/components/DataLoader.java
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -1,5 +1,6 @@
 package com.example.airline_api.components;
 
+import com.example.airline_api.models.Flight;
 import com.example.airline_api.repositories.FlightRepositories;
 import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,12 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        Flight flight1 = new Flight("Peru", 300, "01/01/2001", "00:00");
+        flightRepositories.save(flight1);
+
+        Flight flight2 = new Flight("Poland", 200, "01/02/2002", "00:00");
+        flightRepositories.save(flight2);
+
 
     }
 }
