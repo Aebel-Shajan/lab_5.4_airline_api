79fdd9015480760d9d05d563d91b3c2f22de7320
Aebel Shajan <aebelshajan@gmail.com>
2024-02-29 18:14:15 +0000

    Refactor saveFlight to take FlightDTO as input

--- src/main/java/com/example/airline_api/services/FlightService.java
+++ src/main/java/com/example/airline_api/services/FlightService.java
@@ -1,7 +1,10 @@
 package com.example.airline_api.services;
 
 import com.example.airline_api.models.Flight;
+import com.example.airline_api.models.FlightDTO;
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.FlightRepository;
+import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,9 @@ public class FlightService {
     @Autowired
     FlightRepository flightRepository;
 
+    @Autowired
+    PassengerRepository passengerRepository;
+
     public List<Flight> findAllFlights() {
         return flightRepository.findAll();
     }
@@ -22,7 +28,22 @@ public class FlightService {
         return flightRepository.findById(id);
     }
 
-    public Flight saveFlight(Flight flight) {
+    public Flight saveFlight(FlightDTO flightDTO) {
+
+        Flight flight = new Flight(
+                flightDTO.getDestination(),
+                flightDTO.getCapacity(),
+                flightDTO.getDepartureDate(),
+                flightDTO.getDepartureTime()
+        );
+//        loop through all estateIds
+//        find the estates by their id
+//        add estates to chocolate object
+        for(Long passengerId : flightDTO.getPassengerIds()) {
+            Passenger passenger = passengerRepository.findById(passengerId).get();
+            flight.addPassenger(passenger);
+        }
+//        save chocolate to repo
         return flightRepository.save(flight);
     }
 }
