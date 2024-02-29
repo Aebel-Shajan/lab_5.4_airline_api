30f15eed0af3c9181679fa031939f0d91976a970
Aebel Shajan <aebelshajan@gmail.com>
2024-02-29 18:34:57 +0000

    Add method to check if passenger exists in repository

--- src/main/java/com/example/airline_api/services/PassengerService.java
+++ src/main/java/com/example/airline_api/services/PassengerService.java
@@ -22,4 +22,8 @@ public class PassengerService {
         return passengerRepository.findById(id);
     }
 
+    public boolean passengerExists(long id) {
+        return passengerRepository.existsById(id);
+    }
+
 }
